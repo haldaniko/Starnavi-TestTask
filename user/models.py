af7636d447c1f1bcf32458905f0210073f566c53
haldaniko <haldaniko@gmail.com>
2024-10-19 16:24:27 +0200

    update 'User' model and create related 'UserSettings' model;

--- user/models.py
+++ user/models.py
@@ -1,62 +1,24 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import (
-    AbstractUser,
-    UserManager as DjangoUserManager)
-
-
-class UserManager(DjangoUserManager):
-    """Define a model manager for User model with no username field."""
-
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        if not email:
-            raise ValueError("The given email must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email=email,
-                                 password=password,
-                                 **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(email=email,
-                                 password=password,
-                                 **extra_fields)
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    """User model."""
-
-    username = None
+    username = models.CharField(max_length=128, unique=True)
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='settings')
+    auto_reply_enabled = models.BooleanField(default=False)
+    auto_reply_delay = models.IntegerField(default=10)
+
+    def __str__(self):
+        return f"Settings for {self.user.email}"
