958b29ae6fed12129aad74785ddbdc715cec7dbd
haldaniko <haldaniko@gmail.com>
2024-10-19 14:32:40 +0200

    update serializers for 'Post' and 'Comment'

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,21 +6,27 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = (
+            "id",
             "title",
             "content",
             "author",
+            "is_blocked",
             "created_at",
             "updated_at"
         )
+        read_only_fields = ("author", "is_blocked")
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = (
+            "id",
             "post",
             "content",
             "author",
+            "is_blocked",
             "created_at",
             "updated_at"
         )
+        read_only_fields = ("author", "is_blocked",)
